c36b68c7b3c1eecdf68a82e66e15878754a41a53
LeoCR <leonardoas2007@gmail.com>
2020-06-16 14:38:35 -0600

    Setting up Socket.io inside index.html

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 var PORT = process.env.PORT||3000;
 const express = require('express');
 var app = express();
-var http=require('http').Server(app);
+var http=require('http').createServer(app);
+var io=require('socket.io')(http);
 
 app.use(express.static(__dirname+'/public'));
 
+io.on('connection',function(){
+    console.log('User Connected via socket.io');
+    
+})
+
 http.listen(PORT,function(){
-    console.log('Server Started on Port: '+PORT);
+    console.log('Server Started on http://localhost:'+PORT);
     
 })
