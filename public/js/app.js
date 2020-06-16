3c690bd4177c4fc4ad284e8025b9a4a16f3eb9f5
LeoCR <leonardoas2007@gmail.com>
2020-06-16 15:32:44 -0600

    Added custom  message event

--- public/js/app.js
+++ public/js/app.js
@@ -4,3 +4,7 @@ socket.on('connect',function(){
     console.log('Connected to socket.io server');
     
 })
+socket.on('message',function(message){
+    console.log('New Message');
+    console.log(message.text);    
+})
