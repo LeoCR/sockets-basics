2092588f31be544521cd67bb2ffa626dfc3707ce
LeoCR <leonardoas2007@gmail.com>
2020-06-16 17:16:41 -0600

    Show time on UI

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 var app = express();
 var http=require('http').createServer(app);
 var io=require('socket.io')(http);
+var moment = require('moment');
 
 app.use(express.static(__dirname+'/public'));
 
@@ -12,12 +13,14 @@ io.on('connection',function(socket){
     socket.on('message',function(message){
         console.log('Message reveived: '+message.text);
 
+        message.time=moment().local().format('MMM Do YYYY h:mm a')
         //socket.broadcast.emit('message',message);
         io.emit('message',message);
     });
-
+    //timestamp property = Javascript timestamp (miliseconds)
     socket.emit('message',{
-        text:'Welcome to the web chat application'
+        text:'Welcome to the web chat application',
+        time:moment().local().format('MMM Do YYYY h:mm a')
     })
 })
 
